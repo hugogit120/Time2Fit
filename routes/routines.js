3b130404682de1adc73750116bba28cd9522bfed
hugo <hugomercalibre@gmail.com>
2019-10-30 17:15:38 +0100

    almost ready

--- routes/routines.js
+++ routes/routines.js
@@ -5,34 +5,34 @@ const Exercise = require('../models/Exercise');
 const User = require('../models/User')
 
 //GET routine page:
-router.get('/add', function(req, res, next) {
-  res.render('routines/routine-add'); 
+router.get('/add', function (req, res, next) {
+  res.render('routines/routine-add');
 });
 
 router.post('/add', (req, res, next) => {
-    const { title, description } = req.body;
-    const owner = req.session.currentUser._id;
-    const newRoutine = new Routine({ title, description, owner})
-    newRoutine.save()
+  const { title, description } = req.body;
+  const owner = req.session.currentUser._id;
+  const newRoutine = new Routine({ title, description, owner })
+  newRoutine.save()
     .then((routine) => {
-        console.log(routine);
+      console.log(routine);
       res.redirect('/private/profile');
     })
     .catch((error) => {
       console.log(error);
     })
-  });
+});
 
 //hago get para entrar en la rutina
-router.get('/:id',(req, res, next) => {
+router.get('/:id', (req, res, next) => {
   const { id } = req.params;
-  Routine.findById(id)
+  Routine.findById(id).populate("exercises")
     .then((routine) => {
       User.findById(req.session.currentUser._id)
         .populate('exercises')
-        .then (user => {
+        .then(user => {
           const exercises = user.exercises
-          res.render('routines/routine-detail', {routine, exercises});
+          res.render('routines/routine-detail', { routine, exercises });
         })
         .catch((error) => {
           console.log(error);
@@ -44,23 +44,29 @@ router.get('/:id',(req, res, next) => {
 })
 
 router.post('/:routineId/exercise/:exerciseId/add', (req, res, next) => {
-  const {routineId, exerciseId} = req.params;
-  Routine.findById(routineId).then(routine =>{
-    if(!routine.exercises.includes(exerciseId)){
+  const { routineId, exerciseId } = req.params;
+  Routine.findById(routineId).then(routine => {
+    if (!routine.exercises.includes(exerciseId)) {
       console.log("trueeeee")
-      Routine.findByIdAndUpdate(routineId, { $push: {exercises: exerciseId}}, {new:true})
-      .then(routine => {
-        res.redirect(`/routines/${routineId}`)
-      })
-    }else{
+      Routine.findByIdAndUpdate(routineId, { $push: { exercises: exerciseId } }, { new: true })
+        .then(routine => {
+          res.redirect(`/routines/${routineId}`)
+        })
+    } else {
       res.redirect(`/routines/${routineId}`)
     }
   })
-} )
+})
+
+/* router.get('/exercise/:id', (req, res, next) => {
+  const { id } = req.params;
+  Exercise.findById(id)
+    .then((exercise) => {
+      res.render('private/home-detail', exercise)
+    })
+}) */
 
 
 
 
-
-
-module.exports = router ;
+module.exports = router;
